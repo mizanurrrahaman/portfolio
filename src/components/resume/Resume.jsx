2aa6db5f0eccacae78ffa4cdaa17f28928ee70bf
Murad Hossain Chowdhury <146084384+mizanurrrahaman@users.noreply.github.com>
2024-01-30 21:44:28 -0800

    just update

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -17,7 +17,7 @@ const Resume = () => {
          <Title  title="7+ YEARS OF EXPERIENCE" des="My Resume"/>
       </div>
        <div>
-         <ul className="w-full grid grid-cols-4">
+         <ul className="w-full bg-red-300 grid grid-cols-4">
           
              <li onClick={()=> 
                  setEducationData(true) &
